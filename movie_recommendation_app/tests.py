c3dce3bb62b96ebae939a9f0f7fa87ad37c80ad1
farzanasumona <farzanasumona@gmail.com>
2023-06-10 23:49:12 +0200

    refactored code and added test case

--- movie_recommendation_app/tests.py
+++ movie_recommendation_app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from movie_recommendation_app.views import recommend
 
-# Create your tests here.
+
+class MyModelTestCase(TestCase):
+
+
+    def test_movie_recommendation(self):
+
+        movie_name = 'Iron Man'
+        expected_output = ['Iron Man 2', 'Demolition Man', 'Ant-Man', 'Spider-Man', 'Spider-Man 2']
+
+        actual_output = recommend(movie_name)
+
+        self.assertListEqual(actual_output, expected_output)
+
--- movie_recommendation_app/views.py
+++ movie_recommendation_app/views.py
@@ -3,11 +3,10 @@ from django.shortcuts import render
 from movie_recommendation_app.recommend_model import recommend
 
 def movie_recommendation(request):
-    if 'movie' in request.POST:
+    if request.method == 'POST':
         movie_name = request.POST['movie']
         recommended_movies = recommend(movie_name)
-        if request.method == 'POST':
-            return render(request, 'home.html', {'recommended_movies': recommended_movies})
+        return render(request, 'home.html', {'recommended_movies': recommended_movies})
 
     else:
         return render(request, 'home.html')
